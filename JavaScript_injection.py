4b79f6c72ed3a6d4fd1354dac8a8dd40156a5161
0x1tsjusthicham <hicham.ezzamzami@gmail.com>
2024-10-02 13:40:37 -0400

    Filter For HTTP Request and Response

---
+++ JavaScript_injection.py
@@ -0,0 +1,25 @@
+import netfilterqueue
+import scapy.all as scapy
+
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw) and scapy_packet.haslayer(scapy.TCP):
+        if scapy_packet[scapy.TCP].dport == 80:
+            print("HTTP Request")
+            print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:
+            print("HTTP Request")
+            print(scapy_packet.show())
+    packet.accept()
+
+
+
+
+queue = netfilterqueue.NetfilterQueue()
+
+#0 is the queue number mentionned in iptables command
+queue.bind(0, process_packet)
+
+queue.run()
